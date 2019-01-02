463caed15403911b6a5f10722250a2574ad0f3cc
Eddie <eddie.qiao@geekbang.org>
2019-01-02 16:45:35 +0800

    download a file

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask import send_file, send_from_directory, current_app
+import os
+
 app = Flask(__name__)
+
+# http://localhost:5000/downlaod/a.rar
+@app.route('/download/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    # uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+    uploads = os.path.join(current_app.root_path, './source')
+    return send_from_directory(directory=uploads, filename=filename)
 
 @app.route('/')
 def hello_world():
