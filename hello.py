adeec772ad9314bdb41c5a3aec53a1b91f61f44d
Eddie <eddie.qiao@geekbang.org>
2019-01-04 09:20:22 +0800

    primary ajax

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from flask import send_file, send_from_directory, current_app, render_template
+from flask import send_file, send_from_directory, current_app, render_template, request
 import os
 
 app = Flask(__name__)
+
+app.debug = True
 
 # http://localhost:5000/download/a.rar
 @app.route('/download/<path:filename>', methods=['GET', 'POST'])
@@ -16,3 +18,12 @@ def download(filename):
 def hello_world():
     # return '<html><body><h1>HelloWorld</h1></body></html>'
     return render_template('hello.html')
+
+@app.route('/test', methods=['POST'])
+def down():
+    name = request.form['name']
+    print(name)
+    return 'test succeed'
+
+if __name__ == '__main__':
+    app.run()
